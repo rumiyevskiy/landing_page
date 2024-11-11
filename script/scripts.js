ae7ca017f214f2e948b7310ce6f659a84dd35c90
rumiyevskiy <rumiyevskiy@gmail.com>
2024-11-11 14:55:33 +0200

    modified:   css/style.css

--- script/scripts.js
+++ script/scripts.js
@@ -274,6 +274,36 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
 
+    const swiper = new Swiper('.slider-main-block', {
+        // Optional parameters
+        loop: true,
+      
+        // // If we need pagination
+        // pagination: {
+        //   el: '.swiper-pagination',
+        // },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.slider-main-block__arrow.swiper-button-next',
+          prevEl: '.slider-main-block__arrow.swiper-button-prev',
+        },
+      
+        // // And if we need scrollbar
+        // scrollbar: {
+        //   el: '.swiper-scrollbar',
+        // },
+    
+        autoplay: {
+          delay: 6000,
+        },
+      });
+    
+      swiper.on('click', function (event) {
+        console.log('swiper click', event);
+      });
+
+
 
     // const animItems = document.querySelectorAll ('.anim_items');
     // console.log('animItems: ', animItems );
